29b77339b8da563df99565651e40a70c66c987ac
MadhuNayyar <109290174+MadhuNayyar@users.noreply.github.com>
2022-07-25 17:24:21 +0100

    Update streamlit.app.py
    added menu

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,3 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+Omega 3 & Blueberry
+Calmed
+Kale,
+Spinach & Rocket Smoothie
+Hard-B01 led Free-Range t88
 streamlit.title('My Own New Healthy Dinner')
