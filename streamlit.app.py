4dba15b95557f0c31264ddef5570cdcea3a66f2e
MadhuNayyar <109290174+MadhuNayyar@users.noreply.github.com>
2022-07-25 17:30:53 +0100

    Update streamlit.app.py
    added smoothie

--- streamlit.app.py
+++ streamlit.app.py
@@ -6,3 +6,5 @@ streamlit.header ('🥣 Breakfast Menu')
 streamlit.text ('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('(3 O Build Your Own Fruit Smoothie OB')
