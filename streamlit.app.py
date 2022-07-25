6b3b7172f9baf142ba91cd5cc62154d8453595f3
MadhuNayyar <109290174+MadhuNayyar@users.noreply.github.com>
2022-07-25 17:32:06 +0100

    Update streamlit.app.py
    smoothie icons

--- streamlit.app.py
+++ streamlit.app.py
@@ -7,4 +7,4 @@ streamlit.text ('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🍞 Hard-Boiled Free-Range Egg')
 
-streamlit.header('(3 O Build Your Own Fruit Smoothie OB')
+streamlit.header('(🍌 🥭 Build Your Own Fruit Smoothie 🥝 🍇')
