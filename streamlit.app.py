fe29a2217076b2ada1e77486363dbfb600af8541
MadhuNayyar <109290174+MadhuNayyar@users.noreply.github.com>
2022-08-03 14:42:48 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,10 +1,11 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.title( 'My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.header ('🥣 Breakfast Menu')
@@ -32,6 +33,3 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
-
-
-import snowflake.connector
