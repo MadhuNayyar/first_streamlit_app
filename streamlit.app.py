1bbdebe82f16025db062eb3c62789e9685bf7e59
MadhuNayyar <109290174+MadhuNayyar@users.noreply.github.com>
2022-08-03 17:23:44 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,6 +2,8 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -24,15 +26,16 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header ('Fruityvice Fruit Advice! ') # just writes the data to the screen
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-## streamlit.text(fruityvice_response.json())
-# take the json version of the response and normalize it
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit choice:
+streamlit.error ("Please select a fruit to get information.")
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+streamlit.error)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
